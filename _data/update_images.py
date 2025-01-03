e7c5e52d71bc1338f3c480c3a5ea6b4fbd0dfe1d
kyleqihua <kyleqihuahuang@gmail.com>
2025-01-03 08:16:59 +0800

    update the exact time of photos

--- _data/update_images.py
+++ _data/update_images.py
@@ -14,10 +14,14 @@ for filename in sorted(os.listdir(image_directory)):
             tags = exifread.process_file(img_file)
             date_taken = tags.get('EXIF DateTimeOriginal') or tags.get('Image DateTime')
             if date_taken:
-                date_str = str(date_taken).split(' ')[0].replace(':', '-')
+                date_time_str = str(date_taken)
+                date_part, time_part = date_time_str.split(' ')
+                date_str = date_part.replace(':', '-')
+                time_str = time_part
             else:
                 date_str = 'Unknown Date'
-        image_data.append({'path': filepath, 'date': date_str})
+                time_str = 'Unknown Time'
+        image_data.append({'path': filepath, 'date': date_str, 'time': time_str})
 
 # 写入到 YAML 文件
 with open(output_file, 'w') as file:
