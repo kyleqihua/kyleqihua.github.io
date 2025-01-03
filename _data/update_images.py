f1c0749b96f3f807d3e8423955dc974d67b09705
kyleqihua <kyleqihuahuang@gmail.com>
2025-01-03 10:11:50 +0800

    fixing the format problem again..

--- _data/update_images.py
+++ _data/update_images.py
@@ -1,7 +1,7 @@
 import os
 import yaml
 import exifread
-from datetime import datetime
+from datetime import datetime, timedelta
 
 image_directory = '../images/sunrises'
 output_file = './images.yml'
@@ -13,8 +13,7 @@ for filename in sorted(os.listdir(image_directory)):
         filepath = f"/{image_directory}/{filename}"
         with open(os.path.join(image_directory, filename), 'rb') as img_file:
             tags = exifread.process_file(img_file, details=False)
-            
-            # Get EXIF time data
+
             date_taken = tags.get('EXIF DateTimeOriginal') or tags.get('Image DateTime')
             date_str = 'Unknown Date'
             time_str = 'Unknown Time'
@@ -22,14 +21,32 @@ for filename in sorted(os.listdir(image_directory)):
             if date_taken:
                 date_time_str = str(date_taken).strip()
                 try:
-                    # Convert EXIF time to proper format
                     dt = datetime.strptime(date_time_str, '%Y:%m:%d %H:%M:%S')
                     date_str = dt.strftime('%Y-%m-%d')
-                    # Store time as string in HH:MM:SS format
                     time_str = dt.strftime('%H:%M:%S')
                 except ValueError:
-                    print(f"Warning: Could not parse date/time for {filename}")
-                    continue
+                    pass # If parsing fails, we'll try to get time from SubSecTime
+            else:
+                # If no full date and time, try to extract date from filename if possible
+                parts = filename.split('-')
+                if len(parts) > 2 and all(part.isdigit() and len(part) in [4, 2] for part in parts[:3]):
+                    try:
+                        date_str = f"{parts[0]}-{parts[1]}-{parts[2]}"
+                        datetime.strptime(date_str, '%Y-%m-%d') # Validate date format
+                    except ValueError:
+                        pass  # Keep 'Unknown Date' if filename doesn't match
+
+            # Attempt to get time from SubSecTime only if we don't have a valid time yet
+            if time_str == 'Unknown Time':
+                time_tag = tags.get('EXIF SubSecTimeOriginal') or tags.get('EXIF SubSecTime')
+                if time_tag:
+                    try:
+                        total_seconds = float(str(time_tag))
+                        td = timedelta(seconds=total_seconds)
+                        time_of_day = datetime.min + td
+                        time_str = time_of_day.strftime('%H:%M:%S')
+                    except ValueError:
+                        pass
 
         image_data.append({
             'path': filepath,
@@ -37,9 +54,6 @@ for filename in sorted(os.listdir(image_directory)):
             'time': time_str
         })
 
-# Sort images by date and time
-image_data.sort(key=lambda x: (x['date'], x['time']), reverse=True)
-
-# Write to YAML file
-with open(output_file, 'w') as f:
-    yaml.dump(image_data, f, allow_unicode=True, sort_keys=False)
+# Write to YAML
+with open(output_file, 'w') as file:
+    yaml.dump(image_data, file)
